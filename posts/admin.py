f45f853ddd1406b230a03344e61e4bcc1c0536eb
thedevsaddam <thedevsaddam@gmail.com>
2016-07-27 01:12:33 +0600

    db migrated, super user added, post crud and customizing admin

--- posts/admin.py
+++ posts/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Post
+
+class PostAdmin(admin.ModelAdmin):
+    """customize the model admin"""
+    list_display = ["title", "timestamp", "updated"]
+    # list_display_links = ["updated"]
+    list_filter = ["title", "updated"]
+    search_fields = ["title", "content"]
+    # list_editable = ["title"]
+    class Meta:
+        model = Post
+
+
+
+# register our model to admin dasboard
+admin.site.register(Post, PostAdmin)
--- posts/models.py
+++ posts/models.py
@@ -2,3 +2,21 @@ from django.db import models
 
 # Create your models here.
 
+class Post(models.Model):
+    """"Post model"""
+    title = models.CharField(max_length=120)
+    content = models.TextField()
+    updated = models.DateTimeField(auto_now=True, auto_now_add=False)
+    timestamp = models.DateTimeField(auto_now=False, auto_now_add=True)
+
+    # make the model unicode supported
+    # in Python 2.7
+    # def __unicode__(self):
+    #     return self.title
+
+    def __str__(self):  # Python 3
+        return self.title
+
+
+
+
