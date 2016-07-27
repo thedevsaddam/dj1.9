b87f27174b4628fbf93a86fd99baa8245872cac9
thedevsaddam92 <saddam.hossain@gagagugu.com>
2016-07-27 18:51:31 +0600

    post list showing through template

--- posts/views.py
+++ posts/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from . import models
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse("<h5>Posts list</h5>")
+    posts = models.Post.objects.all()
+    # html =  ""
+    # for post in posts:
+    #     html = html+"<li>"+post.title+"</li>"
+    # return HttpResponse("<ol>"+ html +"</ol>")
+    return render(request, 'index.html', { "posts" : posts})
