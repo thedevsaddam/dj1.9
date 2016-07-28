b45fb514c9d724752b5ffc82484961010abb2f1a
thedevsaddam92 <saddam.hossain@gagagugu.com>
2016-07-28 16:26:52 +0600

    single page added

--- posts/urls.py
+++ posts/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^posts/(?P<id>[0-9]+)/$', views.details, name='details')
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,13 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from . import models
+from .models import Post
+import time
 
 # Create your views here.
 
 def index(request):
+    t = time.time()
     posts = models.Post.objects.all()
     # html =  ""
     # for post in posts:
     #     html = html+"<li>"+post.title+"</li>"
     # return HttpResponse("<ol>"+ html +"</ol>")
-    return render(request, 'index.html', { "posts" : posts})
+    tme = time.time()-t
+    return render(request, 'posts/index.html', {"posts" : posts, "time" : tme})
+
+
+def details(request, id):
+    post = get_object_or_404(Post, pk=id)
+    return render(request, 'posts/details.html', {'post': post})
